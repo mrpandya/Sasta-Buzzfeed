5b9ad96b66123d2df42d597302c6d30d86c88cab
manan <mrpandya2017@gmail.com>
2020-09-03 03:00:40 +0530

    oops

---
+++ cards/static/cards/js/index.js
@@ -0,0 +1,23 @@
+document.getElementById("fire").style.visibility="hidden";
+document.getElementById("btn_text").onclick=function(){fly()};
+function getStarted() {
+//   window.location.replace('../../templates/cards/home.html');
+}
+function fly(){
+    document.getElementById("fire").style.visibility="initial";
+    var spaceship=document.getElementById("fly_rocket");
+    var thrust=document.getElementById("fly_fire");
+    var pos=0;
+    var id=setInterval(frame,2);
+    function frame(){
+        if(pos==-600){
+            clearInterval(id);
+            getStarted();
+        }
+        else{
+            pos--;
+            spaceship.style.transform="translateY("+pos+"px)";
+            thrust.style.transform="translateY("+pos+"px)";
+        }
+    }
+}
---
+++ cards/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name='buzz'
+urlpatterns=[
+    path('',views.IndexView.as_view(),name="index"),
+    path('home',views.HomeView.as_view(),name="home"),
+]
